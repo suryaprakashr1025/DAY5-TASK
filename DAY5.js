f91aca3c8a4904cccc4b61c706aae6d383fdc671
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-08-19 22:58:40 +0530

    Add files via upload

---
+++ DAY5.js
@@ -0,0 +1,213 @@
+//1.anonymous function && IIFE function
+
+//1.print odd number in an array
+let arr=[1,3,4,2,5,8,10]
+let odd=function(){
+    for (let i = 0; i<arr.length; i++){
+        if (arr[i]%2!=0){
+            console.log("The odd number is:"+arr[i]);
+        }
+    }
+}
+odd();
+
+//using IIFE function
+
+(function(){
+    for (let i = 0; i<arr.length; i++){
+        if (arr[i]%2!=0){
+            console.log("The odd number is:"+arr[i]);
+        }
+    }
+})
+();
+
+//using IIFE arrow function
+
+(()=>{
+    for (let i = 0; i<arr.length; i++){
+        if (arr[i]%2!=0){
+            console.log("The odd number is:"+arr[i]);
+        }
+    }
+})
+();
+
+//2.covert all the strings to title caps in a string array
+
+let student=["vijay","surya","ajith","dhanush"];
+
+let caps = function(){
+    for(let i=0; i<student.length; i++){
+        let title=student[i][0].toUpperCase()+ student[i].slice(1);
+        console.log(title);
+    }
+}
+caps();
+
+//using IIFE  function
+(function(){
+    for(let i=0; i<student.length; i++){
+        let title=student[i][0].toUpperCase()+ student[i].slice(1);
+        console.log(title);
+    }
+})
+();
+
+//using IIFE arrow function
+(()=>{
+    for(let i=0; i<student.length; i++){
+        let title=student[i][0].toUpperCase()+ student[i].slice(1);
+        console.log(title);
+    }
+})
+();
+
+//3.sum of all numbers in an array:
+var sum=[10,15,20,100,70];
+var a =0;
+let val=function(){
+    for(let i =0; i<sum.length; i++){
+        
+        a = a+sum[i];
+    }
+    console.log(a);
+}
+val();
+
+//using IIFE  function
+var sum1=[10,15,20,100,70];
+var a1 =0;
+(function(){
+    for(let i =0; i<sum1.length; i++){
+        a1 = a1+sum[i];
+    }
+    console.log(a1);
+})
+();
+
+//using IIFE arrow function:
+var sum2=[10,15,20,100,70];
+var a2 =0;
+(()=>{
+    for(let i =0; i<sum2.length; i++){
+        a2 = a2+sum[i];
+    }
+    console.log(a2);
+})
+();
+
+//4.return all the prime numbers in an array:
+let num=[1,2,3,4,5,23,47,67,48,69,97,66,49,61,75,77,95];
+let prime =function(){
+    for (let i =1; i<num.length; i++){
+       if(num[i]===1){
+        console.log(num[i] +" is not a prime number")
+       }     
+      else if(num[i]/2===1 || num[i]/3===1 || num[i]/5===1 ||num[i]/7===1){
+            console.log( num[i] +" is a prime number");
+        }
+        else if(num[i]%2!=0 && num[i]%3!=0 && num[i]%5!=0 && num[i]%7!=0 ){
+            console.log(num[i] + "is a prime number");
+        }
+    }
+}
+prime();
+
+//using IIFE  function
+(function(){
+    for (let i =1; i<num.length; i++){
+       if(num[i]===1){
+        console.log(num[i] +" is not a prime number")
+       }     
+      else if(num[i]/2===1 || num[i]/3===1 || num[i]/5===1 ||num[i]/7===1){
+            console.log( num[i] +" is a prime number");
+        }
+        else if(num[i]%2!=0 && num[i]%3!=0 && num[i]%5!=0 && num[i]%7!=0 ){
+            console.log(num[i] + "is a prime number");
+        }
+    }
+})
+();
+
+//using IIFE arrow function:
+(()=>{
+    for (let i =1; i<num.length; i++){
+       if(num[i]===1){
+        console.log(num[i] +" is not a prime number")
+       }     
+      else if(num[i]/2===1 || num[i]/3===1 || num[i]/5===1 ||num[i]/7===1){
+            console.log( num[i] +" is a prime number");
+        }
+        else if(num[i]%2!=0 && num[i]%3!=0 && num[i]%5!=0 && num[i]%7!=0 ){
+            console.log(num[i] + "is a prime number");
+        }
+    }
+})
+();
+
+
+//5.return all the palindromes in array:
+
+let palin=["madam","malayalam","nitin","surya"];
+let  fin ="";
+let clear="";
+let palindrome=function(){
+    for (let i=0; i<palin.length; i++){
+            fin =clear;
+        for(let j=palin[i].length-1;j>=0;j--){
+         fin+=palin[i][j]+"";
+        }
+        if (palin[i]===fin){
+      console.log(palin[i] +" "+"is a palindrome");
+        }
+    else{
+         console.log(palin[i] +" "+"is not a palindrome");
+        }
+        }
+    }
+palindrome();
+
+//using IIFE  function:
+(function(){
+    for (let i=0; i<palin.length; i++){
+            fin =clear;
+        for(let j=palin[i].length-1;j>=0;j--){
+         fin+=palin[i][j]+"";
+        }
+        if (palin[i]===fin){
+      console.log(palin[i] +" "+"is a palindrome");
+        }
+    else{
+         console.log(palin[i] +" "+"is not a palindrome");
+        }
+        }
+    })
+        ();
+
+ //using IIFE arrow function:
+    (()=>{
+     for (let i=0; i<palin.length; i++){
+                    fin =clear;
+                for(let j=palin[i].length-1;j>=0;j--){
+                 fin+=palin[i][j]+"";
+                }
+                if (palin[i]===fin){
+              console.log(palin[i] +" "+"is a palindrome");
+                }
+            else{
+                 console.log(palin[i] +" "+"is not a palindrome");
+                }
+                }
+            })
+                ();
+            
+//6.return median of two sorted array of same size:
+
+let array1=[1,5,2,3,8,9];
+let array2=[15,20,7,10,4];
+let add = [array1,...array2];
+let array  =add.sort(function compareFn(a,b){     
+   return a-b;    });
+console.log(add);
+
---
+++ DAY51.js
@@ -0,0 +1,74 @@
+//1.arrow function 
+
+//1.print odd number in an array
+let arr1=[1,3,4,2,5,8,10]
+let odd1=()=>{
+    for (let i = 0; i<arr1.length; i++){
+        if (arr1[i]%2!=0){
+            console.log("The odd number is:"+arr1[i]);
+        }
+    }
+}
+odd1();
+
+//2.covert all the strings to title caps in a string array
+
+let student1=["vijay","surya","ajith","dhanush"];
+
+let caps1 = ()=>{
+    for(let i=0; i<student1.length; i++){
+        let title=student1[i][0].toUpperCase()+ student1[i].slice(1);
+        console.log(title);
+    }
+}
+caps1();
+
+//3.sum of all numbers in an array:
+var sum1=[10,15,20,100,70];
+var a1 =0;
+let val1=()=>{
+    for(let i =0; i<sum1.length; i++){
+        
+        a1 = a1+sum1[i];
+    }
+    console.log(a1);
+}
+val1();
+
+//4.return all the prime numbers in an array:
+let num1=[1,2,3,4,5,23,47,67,48,69,97,66,49,61,75,77,95];
+let prime1 =()=>{
+    for (let i =1; i<num1.length; i++){
+       if(num1[i]===1){
+        console.log(num1[i] +" is not a prime number")
+       }     
+      else if(num1[i]/2===1 || num1[i]/3===1 || num1[i]/5===1 ||num1[i]/7===1){
+            console.log( num1[i] +" is a prime number");
+        }
+        else if(num1[i]%2!=0 && num1[i]%3!=0 && num1[i]%5!=0 && num1[i]%7!=0 ){
+            console.log(num1[i] + "is a prime number");
+        }
+    }
+}
+prime1();
+
+//5.return all the palindromes in array:
+
+let palin1=["madam","malayalam","nitin","surya"];
+let  fin1 ="";
+let clear1="";
+let palindrome1=()=>{
+    for (let i=0; i<palin1.length; i++){
+            fin1 =clear;
+        for(let j=palin1[i].length-1;j>=0;j--){
+         fin+=palin1[i][j]+"";
+        }
+        if (palin1[i]===fin){
+      console.log(palin1[i] +" "+"is a palindrome");
+        }
+    else{
+         console.log(palin1[i] +" "+"is not a palindrome");
+        }
+        }
+    }
+palindrome1();
